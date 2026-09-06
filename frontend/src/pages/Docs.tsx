1341aa27081a047b273972c259375960d36d89ff
abhinav29102005 <abhimamapapa29@gmail.com>
2026-09-07 02:11:36 +0530

    fix: add missing Link import in Docs component

--- frontend/src/pages/Docs.tsx
+++ frontend/src/pages/Docs.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { 
   Database, ShieldCheck, FileText, Code2, Search, 
   Sun, Moon, Printer, Copy, Check, Layers, Cpu, 
